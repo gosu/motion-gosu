bc95b877e6b26d9d31a96fd259ef217a07e22a9d
Julian Raschke <julian@raschke.de>
2015-05-28 22:03:05 +0200

    Refactor tests

--- GosuKitTests/GosuKitTests.m
+++ GosuKitTests/GosuKitTests.m
@@ -38,38 +38,26 @@ static NSString *kBMPTempFilename = @"/tmp/GosuKitTests-Test.bmp";
 - (void)testColorConstructors
 {
     GSKColor *color1 = [GSKColor new];
-    XCTAssert([color1 gl] == 0x00000000,
+    XCTAssertEqual([color1 gl], 0x00000000,
               @"default constructor should nil everything out because this is Objective C");
     
     GSKColor *color2 = [[GSKColor alloc] initWithARGB:0x12345678];
-    XCTAssert(color2.alpha == 0x12,
-              @"getter should return respective byte from constructor");
-    XCTAssert(color2.red == 0x34,
-              @"getter should return respective byte from constructor");
-    XCTAssert(color2.green == 0x56,
-              @"getter should return respective byte from constructor");
-    XCTAssert(color2.blue == 0x78,
-              @"getter should return respective byte from constructor");
+    XCTAssertEqual(color2.alpha, 0x12);
+    XCTAssertEqual(color2.red, 0x34);
+    XCTAssertEqual(color2.green, 0x56);
+    XCTAssertEqual(color2.blue, 0x78);
     
     GSKColor *color3 = [[GSKColor alloc] initWithAlpha:10 red:20 green:30 blue:40];
-    XCTAssert(color3.alpha == 10,
-              @"getter should return respective byte from constructor");
-    XCTAssert(color3.red == 20,
-              @"getter should return respective byte from constructor");
-    XCTAssert(color3.green == 30,
-              @"getter should return respective byte from constructor");
-    XCTAssert(color3.blue == 40,
-              @"getter should return respective byte from constructor");
+    XCTAssertEqual(color3.alpha, 10);
+    XCTAssertEqual(color3.red, 20);
+    XCTAssertEqual(color3.green, 30);
+    XCTAssertEqual(color3.blue, 40);
     
     GSKColor *color4 = [[GSKColor alloc] initWithAlpha:123 hue:180.0 saturation:1.0 value:1.0];
-    XCTAssert(color4.alpha == 123,
-              @"getter should return respective byte from constructor");
-    XCTAssert(color4.red == 0,
-              @"getter should return respective byte from constructor");
-    XCTAssert(color4.green == 0xff,
-              @"getter should return respective byte from constructor");
-    XCTAssert(color4.blue == 0xff,
-              @"getter should return respective byte from constructor");
+    XCTAssertEqual(color4.alpha, 123);
+    XCTAssertEqual(color4.red, 0);
+    XCTAssertEqual(color4.green, 0xff);
+    XCTAssertEqual(color4.blue, 0xff);
 }
 
 - (void)testColorMutability
@@ -77,35 +65,28 @@ static NSString *kBMPTempFilename = @"/tmp/GosuKitTests-Test.bmp";
     GSKColor *color = [[GSKColor alloc] initWithARGB:0x11223344];
     color = [color copy];
     
-    XCTAssert(color.green == 0x33,
-              @"-[GSKColor copy] should return same color");
-    
+    XCTAssertEqual(color.green, 0x33);
     XCTAssert([color isMemberOfClass:[GSKColor class]],
               @"-[GSKColor copy] should return immutable color");
     XCTAssertThrows([(GSKMutableColor *)color setAlpha:4]);
     XCTAssertFalse([color respondsToSelector:@selector(setHue:)]);
     
     GSKMutableColor *mutableColor = [color mutableCopy];
-    XCTAssert([mutableColor isMemberOfClass:[GSKMutableColor class]],
-              @"-[GSKColor mutableCopy] should return mutable color");
+    XCTAssert([mutableColor isMemberOfClass:[GSKMutableColor class]]);
     mutableColor.alpha = 0x55;
-    XCTAssert(mutableColor.alpha == 0x55,
-              @"GSKMutableColor's setters should work");
+    XCTAssertEqual(mutableColor.alpha, 0x55);
     
     color = [mutableColor copy];
     XCTAssert([color isMemberOfClass:[GSKColor class]],
               @"-[GSKMutableColor copy] should return immutable color");
-    XCTAssert(color.alpha == 0x55,
-              @"-[GSKMutableColor copy] should return same color");
+    XCTAssertEqual(color.alpha, 0x55);
 }
 
 - (void)testFont
 {
     GSKFont *font = [[GSKFont alloc] initWithHeight:23 fontName:GSKDefaultFontName() flags:0];
-    XCTAssertEqual(font.name, GSKDefaultFontName(),
-                   @"-[GSKFont name] should return constructor parameter");
-    XCTAssertEqual(font.height, 23,
-                   @"-[GSKFont height] should return constructor parameter");
+    XCTAssertEqualObjects(font.name, GSKDefaultFontName());
+    XCTAssertEqual(font.height, 23);
     XCTAssertEqual([font textWidth:@"Test" scaleX:0], 0,
                    @"the width of every text should be 0 with scaleX=0");
     XCTAssertGreaterThan([font textWidth:@"W" scaleX:1],
@@ -114,18 +95,16 @@ static NSString *kBMPTempFilename = @"/tmp/GosuKitTests-Test.bmp";
     
     // TODO: Find a way to test draw:...
     
-    GSKImage *image = [[GSKImage alloc] initWithFilename:@"Test.png" tileable:NO];
+    GSKImage *image = [[GSKImage alloc] initWithFilename:@"test.png" tileable:NO];
     [font setImage:image forCharacter:'X'];
     XCTAssertEqual([font textWidth:@"X" scaleX:1], 123);
 }
 
 - (void)testImageFile
 {
-    GSKImage *image = [[GSKImage alloc] initWithFilename:@"Test.png" tileable:NO];
-    XCTAssertEqual(image.width, 123,
-                   @"should load image with correct size");
-    XCTAssertEqual(image.height, 123,
-                   @"should load image with correct size");
+    GSKImage *image = [[GSKImage alloc] initWithFilename:@"test.png" tileable:NO];
+    XCTAssertEqual(image.width, 123);
+    XCTAssertEqual(image.height, 123);
     
     NSData *data = [image imageData];
     unsigned char *pixels = (unsigned char *)[data bytes];
@@ -166,13 +145,18 @@ static NSString *kBMPTempFilename = @"/tmp/GosuKitTests-Test.bmp";
 
 - (void)testImageFromTiles
 {
-    NSArray *images = [GSKImage imagesFromTiles:@"Test.png" tileWidth:61 tileHeight:-3 tileable:NO];
+    NSArray *images = [GSKImage imagesFromTiles:@"test.png" tileWidth:61 tileHeight:-3 tileable:NO];
     XCTAssertEqual([images count], 6);
     
     GSKImage *oneImage = images[1];
     XCTAssertEqual(oneImage.width, 61);
     GSKImage *anotherImage = images[2];
     XCTAssertEqual(anotherImage.height, 123 / 3);
+}
+
+- (void)testSample
+{
+    
 }
 
 - (void)testWindow
