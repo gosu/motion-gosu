80b7ddd84fb8eb9c78e10f420d6173d3a13beb97
Julian Raschke <julian@raschke.de>
2015-08-25 01:57:58 +0200

    Fix :name parameter to Font#initialize

--- motion/font.rb
+++ motion/font.rb
@@ -23,7 +23,7 @@ module Gosu
     end
     
     def initialize(height, options = {})
-      initWithHeight(height, fontName: options[:font] || GSKGetDefaultFontName(), flags: 0)
+      initWithHeight(height, fontName: options[:name] || GSKGetDefaultFontName(), flags: 0)
     end
     
     def text_width(text, scale_x = 1)
